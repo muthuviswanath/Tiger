42b0e19d8a4b8717f1c4931a696232623cc78c4f
muthuviswanath <muthuviswanath@outlook.com>
2023-05-30 09:38:23 +0530

    Product_Attributes

--- src/com/rakuten/setterinjection/Products.java
+++ src/com/rakuten/setterinjection/Products.java
@@ -2,4 +2,17 @@ package com.rakuten.setterinjection;
 
 public class Products {
 
+	private int pid;
+	private String pname;
+	private int qty;
+	private double price;
+	public Products(int pid, String pname, int qty, double price) {
+		this.pid = pid;
+		this.pname = pname;
+		this.qty = qty;
+		this.price = price;
+	}
+	
+	
+	
 }
